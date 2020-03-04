440d10aa00f358d9d640ec01af8c58c8c2c59e5b
wlxo0401 <wlxo0401@gmail.com>
2020-03-04 18:30:26 +0900

    리스트 다시

--- corona19.py
+++ corona19.py
@@ -7,13 +7,13 @@ from urllib.request import urlopen
 
 class Main:
     ll = ['확진환자','격리해제','사망자','검사진행'] 
-    numm = []
     print('\n\n코로나 상황을 기록을 하기 편하도록 간단하게 만든 프로그램')
     print('\n\n이 정보는 코로나바이러스감염증-19(COVID-19) 사이트를 통해서 받아옵니다.\n\n\n')
     print('''--------------------------사용법--------------------------''')
     print('''프로그램을 실행 시키고 숫자 1을 치고 엔터만 누르면 됩니다.
     기록은 이 프로그램과 같은 폴더에 메모장으로 남게 됩니다.\n\n''')
     while True:
+        numm = []
         now = datetime.datetime.now()
         i = 0
         val = int(input('==  숫자 1을 입력하고 엔터를 누르면 코로나 정보가 나옵니다. : '))
@@ -26,9 +26,7 @@ class Main:
             timenow = ("%s년 %s월 %s일 %s시 %s분\n" %(now.year, now.month, now.day, now.hour, now.minute))
             print(timenow)
             f1.write(timenow)
-            for anchor in soup.select("td.w_bold"):
-                # print(ll[i] +' : ' + anchor.get_text())
-                # i = i+1
+            for anchor in soup.select('td.w_bold'):
                 numm.append(anchor.get_text())
             for i in range(0,4):
                 print('| '+ll[i]+' : '+numm[i]+' |',end=" ")
@@ -37,9 +35,7 @@ class Main:
             print('\n')
             print('=====================================================================================')
             print('\n')
+            del numm[:] # list 안의 데이터만 삭제
             f1.close()
         else:
-            print('잘 못 누르셨습니다.')       
-
-
-        
+            print('잘 못 누르셨습니다.')
--- test.py
+++ test.py
@@ -1,23 +1,10 @@
-from threading import Thread
-import time
-​
 
-def my_thread(val):
-    for i in range(3):
-        print("I'm Thread")
-        time.sleep(1)
-​
-​
-## 인스턴스 만들기
-## 첫번째 argu는 스레드 함수 이름, 두번째 argu는 매개변수를 튜플 형태로 전달한 것.
-t1 = Thread(target = my_thread, args=(1,))
-​
-## 스레드 시작.
-t1.start()
-t1.join()
-​
-## main
-for i in range(3):
-    print("I'm main Thread")
-    time.sleep(1)
-print("----the end----")
+
+
+test = ['aa','bb','cc']
+
+print(test)
+
+del test[2]
+
+print(test)
